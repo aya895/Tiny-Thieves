406473bb9790d0e91da7899b4c906bb9544b5ea8
Habiba-Saad <223306280+Habiba-saad@users.noreply.github.com>
2026-08-14 22:17:03 +0300

    isa ykon SFX kda kolo 5eles

---
+++ Assets/Scripts/Sounds/ExplosionSound.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class ExplosionSound : MonoBehaviour
+{
+    [SerializeField] private AudioManager audioManager;
+    [SerializeField] private AudioClip explosionClip;
+
+    private void OnEnable()
+    {
+        ExplosionSignal.OnExplosion += HandleExplosion;
+    }
+
+    private void OnDisable()
+    {
+        ExplosionSignal.OnExplosion -= HandleExplosion;
+    }
+
+    private void HandleExplosion(Vector2 position, float radius, float damage)
+    {
+        if (audioManager == null || explosionClip == null)
+            return;
+
+        audioManager.PlaySfx(explosionClip);
+    }
+}
